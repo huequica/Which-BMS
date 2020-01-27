4a367aad4158fb6481f8b0ecd960b84d832f1871
huequica <harumaki1225@gmail.com>
2020-01-27 11:06:52 +0900

    implement to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import get_MetaData from './modules/get_metadata';
 
 const App: React.FC = () => {
+  get_MetaData().then((response: string) => {
+    console.log(response);
+  });
   return(
     <h1>hello world</h1>
   );
