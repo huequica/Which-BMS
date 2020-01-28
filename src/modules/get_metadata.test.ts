de4b3ecac179edff935df713362b1489086f85fd
huequica <harumaki1225@gmail.com>
2020-01-29 01:41:46 +0900

    add testcase

--- src/modules/get_metadata.test.ts
+++ src/modules/get_metadata.test.ts
@@ -4,6 +4,10 @@ test('hogehoge => undefined', (): void => {
   expect(get_MetaData('hoge')).resolves.toBeUndefined();
 });
 
+test('https://huequica.xyz => undefined', (): void => {
+  expect(get_MetaData('https://huequica.xtz')).resolves.toBeUndefined();
+});
+
 test('https://lite.stellabms.xyz/table.html => string', (): void => {
   get_MetaData('https://lite.stellabms.xyz/table.html').then((response: string) => {
     expect(typeof response).toBe('string');
