645396e04ec6f7eedeaaa120dcb73937d77e832c
huequica <harumaki1225@gmail.com>
2020-01-28 19:30:59 +0900

    create error throw

--- src/modules/get_metadata.ts
+++ src/modules/get_metadata.ts
@@ -7,16 +7,25 @@ import axios from "axios";
  * @param string endpoint URL of Dificulty table Page
  */
 const get_MetaData: Function = async (endpoint: string): Promise<string | null | undefined> => {
-  const result = await axios({
-    method: 'GET',
-    url: endpoint,
-    params: {},
-    responseType: 'document'
-  });
-  const html: Document = result.data;
-  const heads:HTMLCollection = html.head.children;
-  const json_url : string | null | undefined = heads.namedItem('bmstable')?.getAttribute('content');
-  return json_url;
+  try{
+    // Get html document from url
+    const result = await axios({
+      method: 'GET',
+      url: endpoint,
+      params: {},
+      responseType: 'document'
+    });
+    // create html document & pick header tag
+    const html: Document = result.data;
+    const heads:HTMLCollection = html.head.children;
+    
+    // return bmsInfo(header) json.
+    // That has`<meta name='bmstable' content='hoge'>`
+    return heads.namedItem('bmstable')?.getAttribute('content');
+  }
+  catch(error) {
+    return error;
+  }
 };
 
 export default get_MetaData;
