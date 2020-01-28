e83ba194fe1bd42cb04ce85c002656a7361908be
huequica <harumaki1225@gmail.com>
2020-01-28 19:16:46 +0900

    remove default URL value

--- src/modules/get_metadata.ts
+++ src/modules/get_metadata.ts
@@ -6,7 +6,7 @@ import axios from "axios";
  * @author huequica
  * @param string endpoint URL of Dificulty table Page
  */
-const get_MetaData: Function = async (endpoint: string = 'https://lite.stellabms.xyz/table.html'): Promise<string | null | undefined> => {
+const get_MetaData: Function = async (endpoint: string): Promise<string | null | undefined> => {
   const result = await axios({
     method: 'GET',
     url: endpoint,
