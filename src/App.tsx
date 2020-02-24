e44005dd2db7a46c9505dc13034f8515f545e951
huequica <harumaki1225@gmail.com>
2020-02-25 00:40:22 +0900

    remove a function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import get_MetaData from './modules/get_metadata';
 
 const App: React.FC = () => {
-  get_MetaData('https://lite.stellabms.xyz/table.html').then((response: string) => {
-    console.log(response);
-  });
   return(
     <h1>hello world</h1>
   );
