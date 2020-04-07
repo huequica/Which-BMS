2acadef5a8376ef8b60aceaa07d42689416f2ce8
huequica <harumaki1225@gmail.com>
2020-04-08 00:05:37 +0900

    remove debug code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import FCC_API from './modules/FCC_API';
 
 const App: React.FC = () => {
-  FCC_API.getHead('http://rattoto10.jounin.jp/header.json').then((res : string) => {
-    console.log(res);
-  });
 
   return(
     <h1>hello world</h1>
